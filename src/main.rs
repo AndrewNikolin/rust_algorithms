ce72816d36d08561c846a1197b467578d827ce3d
Andrew Nikolin <andrew.nikolin.dev@outlook.com>
2020-08-07 11:44:27 +0300

    Insertion sort reversed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
@@ -7,6 +6,12 @@ fn main() {
 
     insertion_sort(&mut test);
 
+    for x in &test {
+        print!("{} ", x);
+    }
+
+    insertion_sort_reversed(&mut test);
+    println!();
     for x in &test {
         print!("{} ", x);
     }
@@ -24,34 +29,14 @@ fn insertion_sort(A: &mut [i32]) {
     }
 }
 
-fn guessing_game() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+fn insertion_sort_reversed(A: &mut [i32]) {
+    for j in 1..A.len() as i32 {
+        let key = A[j as usize];
+        let mut i = j - 1;
+        while i >= 0 && A[i as usize] < key {
+            A[(i+1) as usize] = A[i as usize];
+            i = i - 1;
         }
+        A[(i+1) as usize] = key;
     }
 }

84a2ad1348491bf8332c75c2ff457fe9b90b9f29
Andrew Nikolin <andrew.nikolin.dev@outlook.com>
2020-11-15 20:54:43 +0200

    Merge sort

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 
 fn main() {
     let mut test = [22, 72, 66, 27, 23, 73, 42, 53, 18, 20];
+    let size = test.len();
 
     insertion_sort(&mut test);
 
@@ -11,36 +12,83 @@ fn main() {
     insertion_sort_reversed(&mut test);
 
     print_array(&test);
+
+    merge_sort(&mut test, 0, size - 1);
+
+    print_array(&test);
 }
 
-fn insertion_sort(A: &mut [i32]) {
-    for j in 1..A.len() as i32 {
-        let key = A[j as usize];
-        let mut i = j - 1;
-        while i >= 0 && A[i as usize] > key {
-            A[(i+1) as usize] = A[i as usize];
-            i = i - 1;
-        }
-        A[(i+1) as usize] = key;
+fn merge_sort(a: &mut [i32], low: usize, high: usize) {
+    if low < high {
+        let middle = (low + high) / 2;
+        merge_sort(a, low, middle);
+        merge_sort(a, middle + 1, high);
+        merge(a, low, middle, high);
     }
 }
 
-fn insertion_sort_reversed(A: &mut [i32]) {
-    for j in 1..A.len() as i32 {
-        let key = A[j as usize];
-        let mut i = j - 1;
-        while i >= 0 && A[i as usize] < key {
-            A[(i+1) as usize] = A[i as usize];
-            i = i - 1;
+fn merge(a: &mut [i32], low: usize, middle: usize, high: usize) {
+    let mut left: usize = low;
+    let mut right: usize = middle + 1;
+
+    let mut tmp: Vec<i32> = vec![0; high - low + 1];
+    let mut index = 0;
+
+    while left <= middle && right <= high {
+        if a[left] < a[right] {
+            tmp[index] = a[left];
+            left += 1;
         }
-        A[(i+1) as usize] = key;
+        else {
+            tmp[index] = a[right];
+            right += 1;
+        }
+        index += 1;
+    }
+
+    for i in left..=middle {
+        tmp[index] = a[i];
+        index += 1;
+    }
+
+    for i in right..=high {
+        tmp[index] = a[i];
+        index += 1;
+    }
+
+    for i in 0..tmp.len() {
+        a[low+i] = tmp[i];
     }
 }
 
-fn print_array(A: &[i32]) {
+fn insertion_sort(a: &mut [i32]) {
+    for j in 1..a.len() as i32 {
+        let key = a[j as usize];
+        let mut i = j - 1;
+        while i >= 0 && a[i as usize] > key {
+            a[(i+1) as usize] = a[i as usize];
+            i = i - 1;
+        }
+        a[(i+1) as usize] = key;
+    }
+}
+
+fn insertion_sort_reversed(a: &mut [i32]) {
+    for j in 1..a.len() as i32 {
+        let key = a[j as usize];
+        let mut i = j - 1;
+        while i >= 0 && a[i as usize] < key {
+            a[(i+1) as usize] = a[i as usize];
+            i = i - 1;
+        }
+        a[(i+1) as usize] = key;
+    }
+}
+
+fn print_array(a: &[i32]) {
     println!();
 
-    for x in A {
+    for x in a {
         print!("{} ", x);
     }
 }
